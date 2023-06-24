bed9868428adc8c8347698a41776b7a170ad1296
Sonicfury <theo.lambrt@gmail.com>
2023-06-24 17:00:43 +0200

    feat: supabase channel

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,52 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/models/order';
 import { AuthService } from 'src/app/services/auth.service';
-import { OrdersService } from 'src/app/services/orders.service';
+import { OrdersService, State } from 'src/app/services/orders.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.scss']
+  styleUrls: ['./orders.component.scss'],
 })
 export class OrdersComponent implements OnInit {
   orders: Order[] = [];
+  state$!: typeof this._ordersService.state$;
   deliveredOrders: Order[] = [];
 
   constructor(
     private _auth: AuthService,
     private _ordersService: OrdersService,
     private _snackbar: SnackbarService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
-    this._ordersService.getByEmployeeId(this._auth.session?.user?.id as string)
-    .pipe(
-    ).subscribe(orders => {
-      this.orders = orders?.filter(o => o.state === OrdersService.ORDER_STATE.TO_DELIVER);
-      this.deliveredOrders = orders?.filter(o => o.state === OrdersService.ORDER_STATE.DONE);
-    })
+    this._ordersService.state$.subscribe((orders) => {
+      this.orders = orders.TO_DELIVER;
+      this.deliveredOrders = orders.DONE;
+    });
   }
 
-  markAs(action: string, order: Order) {
-    const ACTIONS = {
-      DELIVERED: 'delivered',
-      TO_DELIVER: 'to_deliver'
-    }
-    let message = '';
-
-    if (action === ACTIONS.DELIVERED) {
-        this.deliveredOrders = [order, ...this.deliveredOrders]
-        this.orders = this.orders.filter(o => o.id !== order.id)
-        this._snackbar.success(`La commande ${order.id} a été marquée comme livrée.`);
-        return;
-      }
-    if (action === ACTIONS.TO_DELIVER) {
-        this.orders = [order, ...this.orders]
-        this.deliveredOrders = this.deliveredOrders.filter(o => o.id !== order.id)
-        this._snackbar.warning(`La commande ${order.id} a été marquée comme à livrer.`);
-        return;
-      }
+  markAs(state: State, order: Order) {
+    this._ordersService
+      .markAs(state, order)
+      .subscribe((id) =>
+        this._snackbar.info(`La commande ${id} a été mise à jour.`)
+      );
   }
 }
--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,29 +1,142 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {
+  RealtimeChannel,
+  RealtimePostgresChangesPayload,
+} from '@supabase/supabase-js';
+import { BehaviorSubject, from, Observable, of } from 'rxjs';
+import { map, tap, switchMap } from 'rxjs/operators';
 import { Order } from '../models/order';
+import { AuthService } from './auth.service';
 import { SupabaseService } from './supabase.service';
 
+export type OrdersState = Record<
+  keyof typeof OrdersService.ORDER_STATE,
+  Order[]
+>;
+export type State = keyof typeof OrdersService.ORDER_STATE;
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrdersService {
   public static readonly ORDER_STATE = {
-    TO_DELIVER: "3",
-    DONE: "4",
+    TO_DELIVER: '3',
+    DONE: '4',
+  };
+
+  private _channel!: RealtimeChannel;
+  private _state$!: BehaviorSubject<OrdersState>;
+
+  constructor(
+    private _supabaseService: SupabaseService,
+    private _auth: AuthService
+  ) {}
+
+  markAs(state: State, order: Order): Observable<number> {
+    return from(
+      this._supabaseService.supabase
+        .from('orders')
+        .update({ state: OrdersService.ORDER_STATE[state] })
+        .eq('id', order.id)
+    ).pipe(switchMap((_) => of(order.id)));
   }
 
-  constructor(private _supabaseService: SupabaseService) { }
-
-  getByEmployeeId(employeeId: string): Observable<Order[]>{
+  private _getByEmployeeId(employeeId: string): Observable<Order[]> {
     return from(
-      this._supabaseService.supabase.from('orders')
+      this._supabaseService.supabase
+        .from('orders')
         .select()
         .eq('employee_id', Number(employeeId))
         .eq('isTakeaway', true)
         .order('updated_at', { ascending: false })
-    ).pipe(
-      map(({ data }) => data as Order[])
-    )
+    ).pipe(map(({ data }) => data as Order[]));
+  }
+
+  private _handleEvent(
+    payload: RealtimePostgresChangesPayload<{ [key: string]: any }>
+  ) {
+    const current = this._state$.value;
+
+    if (payload.eventType === 'DELETE') {
+      const newState: OrdersState = {
+        TO_DELIVER: current.TO_DELIVER.filter(
+          (td) => td.id !== payload.old['id']
+        ),
+        DONE: current.DONE.filter((td) => td.id !== payload.old['id']),
+      };
+      this._state$.next(newState);
+
+      return;
+    }
+
+    if (payload.eventType === 'INSERT') {
+      const order = payload.new as Order;
+      if (!order.isTakeaway) return;
+
+      const newState = { ...current };
+      const orderState = Object.entries(OrdersService.ORDER_STATE).find(
+        ([k, v]) => v === order.state
+      );
+      if (!orderState) return;
+
+      const key = orderState[0] as keyof typeof OrdersService.ORDER_STATE;
+      newState[key] = [order, ...newState[key]];
+      this._state$.next(newState);
+
+      return;
+    }
+
+    if (payload.eventType === 'UPDATE') {
+      const order = payload.new as Order;
+      if (!order.isTakeaway) return;
+
+      const newState: OrdersState = {
+        TO_DELIVER: current.TO_DELIVER.filter(
+          (td) => td.id !== payload.old['id']
+        ),
+        DONE: current.DONE.filter((td) => td.id !== payload.old['id']),
+      };
+      const orderState = Object.entries(OrdersService.ORDER_STATE).find(
+        ([k, v]) => v === order.state
+      );
+      if (!orderState) return;
+
+      const key = orderState[0] as keyof typeof OrdersService.ORDER_STATE;
+      newState[key] = [order, ...newState[key]];
+      this._state$.next(newState);
+
+      return;
+    }
+  }
+
+  get state$(): Observable<OrdersState> {
+    const userId = this._auth.session?.user?.id;
+    const filter = `employee_id=eq.${userId}`;
+    this._channel = this._supabaseService.supabase
+      .channel('orders_change')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'orders', filter },
+        (payload) => this._handleEvent(payload)
+      );
+
+    return this._getByEmployeeId(userId as string).pipe(
+      tap((orders: Order[]) => {
+        const toDeliver = orders?.filter(
+          (o) => o.state === OrdersService.ORDER_STATE.TO_DELIVER
+        );
+        const delivered = orders?.filter(
+          (o) => o.state === OrdersService.ORDER_STATE.DONE
+        );
+
+        this._state$ = new BehaviorSubject({
+          TO_DELIVER: toDeliver,
+          DONE: delivered,
+        });
+
+        this._channel.subscribe();
+      }),
+      switchMap((_) => this._state$)
+    );
   }
 }
